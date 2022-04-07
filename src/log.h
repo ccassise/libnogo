7f39823282492d869acdeabaf044580cd41d489c
Cole Cassise <cole.cassise@outlook.com>
2022-04-06 21:46:17 -0500

    Remove unused file

--- src/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOG_H_
-#define LOG_H_
-
-#include <stdio.h>
-
-#define LOG_DEBUG(...) printf("[DEBUG] " __VA_ARGS__)
-#define LOG_ERROR(...) fprintf(stderr, "[ERROR] " __VA_ARGS__) 
-
-#endif
